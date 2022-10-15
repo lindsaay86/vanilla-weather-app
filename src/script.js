17c4b2ccf21d8cc05228d87f6da9f2ecaf0012dc
lindsaay86 <lindsay.eunji.kim@gmail.com>
2022-10-15 17:21:16 -0400

    Removed console log and modified date and time elements

--- src/script.js
+++ src/script.js
@@ -1,15 +1,62 @@
+function formatDate(timestamp) {
+  let date = new Date(timestamp);
+  let currentDate = date.getDate();
+  let allWeekdays = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let allMonths = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
+  let month = allMonths[date.getMonth()];
+  let day = allWeekdays[date.getDay()];
+  return `${day}, ${month} ${currentDate}`;
+}
+
+function formatTime(timestamp) {
+  let date = new Date(timestamp);
+  let hours = date.getHours();
+  let minutes = date.getMinutes();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  return `${hours}:${minutes}`;
+}
+
 function displayTemperature(response) {
-  console.log(response.data);
   let temperatureElement = document.querySelector("#temperature");
   let cityElement = document.querySelector("#current-city");
   let descriptionElement = document.querySelector("#weather-description");
   let humidityElement = document.querySelector("#humidity");
   let windSpeedElement = document.querySelector("#windSpeed");
+  let dateElement = document.querySelector("#current-day");
+  let timeElement = document.querySelector("#current-time");
   temperatureElement.innerHTML = Math.round(response.data.main.temp);
   cityElement.innerHTML = response.data.name;
   descriptionElement.innerHTML = response.data.weather[0].description;
   humidityElement.innerHTML = response.data.main.humidity;
   windSpeedElement.innerHTML = Math.round(response.data.wind.speed);
+  dateElement.innerHTML = formatDate(response.data.dt * 1000);
+  timeElement.innerHTML = formatTime(response.data.dt * 1000);
 }
 
 let apiKey = "af7c945ddaf0df80051c18f5499ef66c";
